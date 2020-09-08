5dbc752cf5a8c4ae68638daf72c3c81e2841c0bb
Víctor M. Rivas <vrivas@ujaen.es>
2020-09-08 17:11:54 +0200

    Añado actividad Espana y evento al hacer onClick en botón España de MainActivity

--- app/src/main/java/es/vrivas/paisesintentkotlin/MainActivity.kt
+++ app/src/main/java/es/vrivas/paisesintentkotlin/MainActivity.kt
@@ -1,11 +1,19 @@
 package es.vrivas.paisesintentkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun espana_click(view: View) {
+
+        val i = Intent(this, EspanaActivity::class.java)
+        startActivity(i)
     }
 }
