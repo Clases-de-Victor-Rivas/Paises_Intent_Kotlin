2aac23c020d13bb0ab1a64443469d4635a87e802
Victor Rivas <vrivas@ujaen.es>
2020-11-15 00:07:40 +0100

    Utilizo excepciones para capturar errores en los intent

--- app/src/main/java/es/vrivas/paisesintentkotlin/MainActivity.kt
+++ app/src/main/java/es/vrivas/paisesintentkotlin/MainActivity.kt
@@ -1,9 +1,11 @@
 package es.vrivas.paisesintentkotlin
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +21,14 @@ class MainActivity : AppCompatActivity() {
      */
     @Suppress("UNUSED_PARAMETER")
     fun espana_click(view: View) {
-        val i = Intent(this, EspanaActivity::class.java)
-        startActivity(i)
+        try {
+            val i = Intent(this, EspanaActivity::class.java)
+            startActivity(i)
+        } catch ( e: ActivityNotFoundException ){
+            Snackbar.make( view,
+                "espana_click: Activity Not Found",
+                Snackbar.LENGTH_LONG ).show()
+        }
     }
 
 
@@ -30,13 +38,19 @@ class MainActivity : AppCompatActivity() {
      */
     @Suppress("UNUSED_PARAMETER")
     fun francia_click(view: View) {
-        val i = Intent(this, PaisActivity::class.java)
-        i.putExtra("NOMBRE-PAIS", getString(R.string.francia))
-        i.putExtra("EXTENSION-PAIS", 675_417)
-        i.putExtra( "POBLACION-PAIS", 6_7028_048)
-        i.putExtra("OTROS-DATOS-PAIS", arrayOf<String>("Francés", "Euro", "París"))
+        try {
+            val i = Intent(this, PaisActivity::class.java)
+            i.putExtra("NOMBRE-PAIS", getString(R.string.francia))
+            i.putExtra("EXTENSION-PAIS", 675_417)
+            i.putExtra( "POBLACION-PAIS", 6_7028_048)
+            i.putExtra("OTROS-DATOS-PAIS", arrayOf<String>("Francés", "Euro", "París"))
 
-        startActivity(i)
+            startActivity(i)
+        } catch ( e: ActivityNotFoundException ){
+            Snackbar.make( view,
+                "francia_click: Activity Not Found",
+                Snackbar.LENGTH_LONG ).show()
+        }
     }
 
 
@@ -46,12 +60,19 @@ class MainActivity : AppCompatActivity() {
     */
     @Suppress("UNUSED_PARAMETER")
     fun portugal_click(view: View) {
-        val i = Intent(this, PaisActivity::class.java)
-        i.putExtra(PARAM_COUNTRY_NAME, getString(R.string.portugal))
-        i.putExtra(PARAM_EXTENSION, 92_090)
-        i.putExtra(PARAM_POPULATION, 10_562_178)
-        i.putExtra(PARAM_OTHERS, arrayOf<String>("Portugués", "Euro", "Lisboa"))
-        startActivity(i)
+        try {
+            val i = Intent(this, PaisActivity::class.java)
+            i.putExtra(PARAM_COUNTRY_NAME, getString(R.string.portugal))
+            i.putExtra(PARAM_EXTENSION, 92_090)
+            i.putExtra(PARAM_POPULATION, 10_562_178)
+            i.putExtra(PARAM_OTHERS, arrayOf<String>("Portugués", "Euro", "Lisboa"))
+            startActivity(i)
+        } catch ( e: ActivityNotFoundException ){
+            Snackbar.make( view,
+                "portugal_click: Activity Not Found",
+                Snackbar.LENGTH_LONG ).show()
+        }
+
     }
 
     companion object MAIN_ACTIVITY{
